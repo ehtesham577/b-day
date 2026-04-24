505a0dee250fd3967a2c1116e3c42a4a857230c0
ehtesham577 <ehtesham.mohammed577@gmail.com>
2026-04-24 01:07:41 -0700

    Update script.js

--- script.js
+++ script.js
@@ -79,7 +79,7 @@ function toggleMusic() {
 
 function handleYesClick() {
     ycount++;
-    if (!runawayEnabled) {
+    if (!runawayEnabled && yesTeasedCount<=10) {
         // Tease her to try No first
         const msg = yesMessages[Math.min(yesTeasedCount, yesMessages.length - 1)]
         yesTeasedCount++
@@ -114,8 +114,8 @@ function handleNoClick() {
     // Grow the Yes button bigger each time
     const currentSize = parseFloat(window.getComputedStyle(yesBtn).fontSize)
     yesBtn.style.fontSize = `${currentSize * 1.35}px`
-    const padY = Math.min(18 + noClickCount * 2, 40)
-    const padX = Math.min(45 + noClickCount * 5, 80)
+    const padY = Math.min(5 + noClickCount * 2, 40)
+    const padX = Math.min(8 + noClickCount * 5, 80)
     yesBtn.style.padding = `${padY}px ${padX}px`
 
     // Shrink No button to contrast
@@ -129,10 +129,10 @@ function handleNoClick() {
     swapGif(gifStages[gifIndex])
 
     // Runaway starts at click 5
-   /* if (noClickCount >= 5 && !runawayEnabled) {
+    if (noClickCount >= 5 && !runawayEnabled) {
         enableRunaway()
         runawayEnabled = true
-    }*/
+    }
 }
 
 function swapGif(src) {
